36e2a86ff4cb875ecbb120a7f6a24c34f6df1dab
Azmine <mdbinmonjurazmine@gmail.com>
2025-11-07 16:59:58 +0600

    Ready to deploy backend

--- server/sever.js
+++ server/sever.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 // import { connect } from 'mongoose';
 import connectDB from './configs/db.js';
+import {inngest, functions} from './inggest/index.js';
+import {serve} from 'inngest/express'
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res)=> res.send('Server is running'));
+app.use('/api/inngest', serve({ client: inngest, functions}));
 
 const PORT = process.env.PORT || 4000;
 
